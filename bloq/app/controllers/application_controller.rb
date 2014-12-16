f9ea388b7f87db2ebf7a84b476b8f6f7751ee718
Ji Lee <gafrong@gmail.com>
2014-12-16 16:58:17 +1100

    materialize

--- bloq/app/controllers/application_controller.rb
+++ bloq/app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
 
+  before_action :set_headers
 
   def index
 
@@ -16,6 +17,19 @@ class ApplicationController < ActionController::Base
     respond_to do |format|
       format.html {}
       format.json { render :json => @show_all }
+    end
+  end
+
+  def set_headers
+    if request.headers["HTTP_ORIGIN"]
+    # better way check origin
+    # if request.headers["HTTP_ORIGIN"] && /^https?:\/\/(.*)\.some\.site\.com$/i.match(request.headers["HTTP_ORIGIN"])
+      headers['Access-Control-Allow-Origin'] = request.headers["HTTP_ORIGIN"]
+      headers['Access-Control-Expose-Headers'] = 'ETag'
+      headers['Access-Control-Allow-Methods'] = 'GET, POST, PATCH, PUT, DELETE, OPTIONS, HEAD'
+      headers['Access-Control-Allow-Headers'] = '*'
+      headers['Access-Control-Max-Age'] = '86400'
+      headers['Access-Control-Allow-Credentials'] = 'true'
     end
   end
   
