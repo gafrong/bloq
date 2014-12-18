a2d200c57ff02bf80a241501ab574e04f9bcfc70
Ji Lee <gafrong@gmail.com>
2014-12-18 14:54:28 +1100

    advise

--- bloq/app/controllers/application_controller.rb
+++ bloq/app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
 
   before_action :set_headers
-
+  before_action :authenticate_user!
+  
   def index
     
     @designs  = Design.all
