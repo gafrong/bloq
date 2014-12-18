bcc6aea14432fd6ac664bc16be7f911db6a3600e
Ji Lee <gafrong@gmail.com>
2014-12-18 15:09:01 +1100

    advise test

--- bloq/config/environments/production.rb
+++ bloq/config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Required for Devise to change to production host. 
-  config.action_mailer.default_url_options = { :host => 'http://bloq.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
